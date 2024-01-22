c86b8ac783e86daa73d95d8f8746037b101bb2df
Matteo Valenzi <matteo.valenzi@outlook.com>
2024-01-22 12:06:06 +0100

    .

--- src/app.ts
+++ src/app.ts
@@ -6,25 +6,43 @@ const port = 3000;
 const host = "localhost";
 
 const requestListener = (req: IncomingMessage, res: ServerResponse) => {
-    const u = url.parse(req.url!, true);
     console.log("Richiesta:", req.method, req.url, new Date().toLocaleString());
 
-    const nome = u.query.nome as string;
+    try {
+        const u = url.parse(req.url!, true);
+        const nome = u.query.nome as string;
 
-    switch (u.pathname) {
-        case "/":
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.write("<html><head><title>Ciao!</title></head><body><h1>Ciao [[nome]]!</h1></body></html>".replace("[[nome]]", nome ?? "a tutti"));
-            res.end();
-            break;
-        case "/welcome":
-            res.writeHead(200, { "Content-Type": "text/html" });
-            let html = readFileSync("./templates/welcome.html", "utf8");
-            res.write(html.replace("{{name}}", nome ?? "a tutti"));
-            res.end();
-            break;
-        default:
-            break;
+        switch (u.pathname) {
+            case "/":
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.write("<html><head><title>Ciao!</title></head><body><h1>Ciao [[nome]]!</h1></body></html>".replace("[[nome]]", nome ?? "a tutti"));
+                res.end();
+                break;
+            case "/welcome":
+                let html = readFileSync("./templates/welcome.html", "utf8");
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.write(html.replace("{{name}}", nome ?? "a tutti"));
+                res.end();
+                break;
+            case "/api/v2/mario":
+                const mario = {
+                    nome: "Mario",
+                    cognome: "Rossi"
+                }
+
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.end(JSON.stringify(mario));
+                break;
+            default:
+                res.writeHead(404);
+                res.end("Pagina non trovata");
+                break;
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.writeHead(500);
+        res.end("Errore del server. Contattare l'amministratore di sistema.");
     }
 }
 
